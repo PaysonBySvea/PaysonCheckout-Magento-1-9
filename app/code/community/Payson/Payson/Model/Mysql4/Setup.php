c86ca7481ade9d5d4a1659e4bb4e223991bdf7c8
oscar-vil <oscar.villegas@payson.se>
2016-12-06 15:07:58 +0100

    Removed a folder - Payson.

--- app/code/community/Payson/Payson/Model/Mysql4/Setup.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class Payson_Payson_Model_Mysql4_Setup extends Mage_Sales_Model_Mysql4_Setup {
-    
-}
-
--- app/code/community/Payson/Payson/sql/payson_setup/mysql4-upgrade-0.1.0-0.1.2.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$this->startSetup();
-$this->run('alter table `' . $this->getTable('payson_order') . '` add store_id int;
-');
-
-$this->endSetup();
-
